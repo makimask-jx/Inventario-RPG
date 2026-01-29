e8f9f5c4ddf6a3695ddcf12c0e4cb70531a32654
Jinx <jinxynet@duck.com>
2026-01-29 16:50:55 +0100

    feat: Añadir tipado estatico. Añadir menú inicial

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from collections import Counter, defaultdict
 
 # Colores para la consola
-ROJO = "\033[31m"
-VERDE = "\033[32m"
-AZUL = "\033[34m"
-MAGENTA = "\033[35m"
-RESET = "\033[0m"
+ROJO: str = "\033[31m"
+VERDE: str = "\033[32m"
+AZUL: str = "\033[34m"
+MAGENTA: str = "\033[35m"
+RESET: str = "\033[0m"
 
 # Datos del Héroe
-personaje = {
+personaje: dict = {
     "nombre": "Eldrin",
     "clase": "Hechicero",
     "nivel": 5,
@@ -25,3 +25,32 @@ personaje = {
         {"nombre": "Curación Menor", "dano": -20, "coste": 15, "elemento": "Luz"},
     ],
 }
+
+
+opciones: list = [
+    "Ver estado del Héroe",
+    "Encontrar Cofre",
+    "Consumir Objeto",
+    "Grimorio de hechizos",
+    "Tasar inventario",
+    "Batalla y Analsis",
+    "Salir",
+]
+
+while True:
+    print(f"{AZUL}--- Gestor RPG ---{RESET}")
+    for opcion in opciones:
+        print(f"{opciones.index(opcion)}. {opcion}")
+    print(f"{AZUL}-{RESET}" * 18)
+
+    try:
+        comando: int = int(input("⚔️ Acción a realizar: "))
+
+        match comando:
+            case 6:
+                print(f"{MAGENTA}\n¡Adiós Eldrin!{RESET}")
+                break
+            case _:
+                print(f"{ROJO}\n❓Comando no encontrado {RESET}❓")
+    except ValueError:
+        print(f"{ROJO}☠️ La acción a realizar debe ser un numero del menú ☠️{RESET}")
